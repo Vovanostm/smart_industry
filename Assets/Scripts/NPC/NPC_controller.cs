c94e019d95a8692c38a37a96bf444ca61c3ecd4f
vovanostm <axb48@yandex.ru>
2019-06-11 07:41:27 +0300

    init commit

---
+++ Assets/Scripts/NPC/NPC_controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_controller : MonoBehaviour
+{
+    public GameObject path;
+    public NavMeshAgent agent;
+
+    public Transform waypoints;
+
+    public int waypointNum = 0;
+
+    void Start()
+    {
+        path = GameObject.Find("Path");
+        waypoints = path.transform;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.destination = waypoints.GetChild(0).position;
+    }
+
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.5f)
+        {
+            waypointNum++;
+            if (waypointNum < waypoints.childCount)
+            {
+                agent.destination = waypoints.GetChild(waypointNum).position;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
